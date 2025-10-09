ed43a212248587ed6ad182709ee990a6b2b2dcda
pepevazquez-4 <23040139@alumno.utc.edu.mx>
2025-10-09 17:45:50 -0600

    algo x

--- app-angular/src/app/app.component.ts
+++ app-angular/src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-// Importamos tu componente de perfil aquí
 import { PerfilCardComponent } from './perfil-card/perfil-card.component'; 
 
 @Component({
